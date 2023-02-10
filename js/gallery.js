d81afde0e9891aafacd6c9707bf86db3e5ca1094
523MaximZ <92735775+523MaximZ@users.noreply.github.com>
2023-02-10 11:59:31 -0500

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -33,11 +33,29 @@ function animate() {
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
 
 function swapPhoto() {
-	//Add code here to access the #slideShow element.
-	//Access the img element and replace its source
-	//with a new image from your images array which is loaded 
-	//from the JSON string
-	console.log('swap photo');
+	if(mCurrentIndex >= mImages.length){
+	mCurrentIndex = 0;
+}
+
+	if(mCurrentIndex < 0){
+		mCurrentIndex = mImages.length-1;
+	}
+
+
+document.getElementById('photo').src = mImages[mCurrentIndex].img
+    var loc = document.getElementsByClassName('location')
+    loc[0].innerHTML = 'Location: ' + mImages[mCurrentIndex].location
+	var des = document.getElementsByClassName('description')
+    des[0].innerHTML = 'Description: ' + mImages[mCurrentIndex].description
+    var dt = document.getElementsByClassName('date')
+    dt[0].innerHTML = 'Date: ' + mImages[mCurrentIndex].date
+
+	mLastFrameTime = 0
+	mCurrentIndex += 1
+
+
+	
+
 }
 
 // Counter for the mImages array
@@ -54,35 +72,104 @@ var mJson;
 
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-var mUrl = 'insert_url_here_to_image_json';
+var mUrl = 'https://api.npoint.io/2e8dc462c70cd5f10ca2';
+
+function fetchJSON() {
+	mRequest.onreadystatechange = function() {
+		console.log("on ready state change");
+		if(this.readyState == 4 && this.status == 200){
+			mJson = JSON.parse(mRequest.responseText);
+			iterateJSON(mJson);
+		}
+	}
+	mRequest.open("GET", mUrl, true);
+	mRequest.send();
+
+
+}
+
+function iterateJSON(mJson) {
+	for (x = 0; x < mJson.images.length; x++) {
+		mImages[x] = new GalleryImage();
+		mImages[x].location = mJson.images[x].imgLocation
+		mImages[x].description = mJson.images[x].description
+		mImages[x].date = mJson.images[x].date
+		mImages[x].img = mJson.images[x].imgPath
+		
+	}
+}
+ 
 
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
 //@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
+
 function makeGalleryImageOnloadCallback(galleryImage) {
-	return function(e) {
-		galleryImage.img = e.target;
-		mImages.push(galleryImage);
-	}
+    return function(e) {
+        galleryImage.img = e.target;
+        mImages.push(galleryImage);
+    }
 }
 
 $(document).ready( function() {
-	
-	// This initially hides the photos' metadata information
-	$('.details').eq(0).hide();
-	
-});
+    fetchJSON();
+$( ".moreIndicator" ).click(function() {
+
+
+    if ($(this).hasClass("rot90")) {
+        $(this).removeClass("rot90");
+        $(this).addClass("rot270");
+        $( ".details" ).slideToggle( "slow" );
+
+      } else {
+        $(this).removeClass("rot270");
+        $(this).addClass("rot90");
+        $( ".details" ).slideToggle( "slow" );
+
+      }
+    });
+  });
 
 window.addEventListener('load', function() {
-	
-	console.log('window loaded');
-
+    
+console.log('window loaded');
+$("#imageID").show();
 }, false);
 
 function GalleryImage() {
-	//implement me as an object to hold the following data about an image:
-	//1. location where photo was taken
-	//2. description of photo
-	//3. the date when the photo was taken
-	//4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
+var location;
+var description;
+var data;
+var img;
 }
+
+$(document).ready(function() {
+    $("#nextPhoto").click(function() {
+        swapPhoto();
+    });
+  });
+
+  $(document).ready(function() {
+    $("#prevPhoto").click(function() {
+
+if(mCurrentIndex  >= mImages.length) {
+
+mCurrentIndex = 0;
+}
+
+if(mCurrentIndex  < 0) {
+mCurrentIndex = mImages.length-1;
+}
+
+mLastFrameTime =0;
+mCurrentIndex -=1;
+    });
+  });
+
+function GalleryImage() {
+    var location = '';
+    var description = '';
+    var date = '';
+    var source = '';
+}
+
